8cb6142ae4fad44224ad9e0726dfa163c714de7d
Thomas Gelf <thomas@gelf.net>
2024-10-14 12:26:49 +0200

    composer: add flags, bin: collect vendor images

--- bin/refresh-public.sh
+++ bin/refresh-public.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 find -L vendor/*/*/public/css -type f -name "*.less" -exec cat {} \; 2>/dev/null > public/css/combined.less
 find -L vendor/*/*/public/js -type f -name "*.js" -exec cat {} \; 2>/dev/null > public/js/combined.js
+# TODO: this does not clean up vanished files
+cp -r vendor/*/*/public/img/* public/img/vendor
