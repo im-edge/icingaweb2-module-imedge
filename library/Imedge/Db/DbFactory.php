97d0511b49fe2f901a10bdaecaff4ff791f7f458
Thomas Gelf <thomas@gelf.net>
2024-10-16 17:34:13 +0200

    DbFactory: mostly for Hooks

---
+++ library/Imedge/Db/DbFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Icinga\Module\Imedge\Db;
+
+use gipfl\ZfDb\Adapter\Pdo\PdoAdapter as Db;
+use Icinga\Application\Config;
+use Icinga\Exception\ConfigurationError;
+use Icinga\Module\Imedge\Config\Defaults;
+use Icinga\Module\Imedge\Config\IcingaResource;
+
+class DbFactory
+{
+    protected static ?Db $db = null;
+
+    public static function db(): Db
+    {
+        if (self::$db === null) {
+            if ($name = Config::module(Defaults::MODULE_NAME)->get('db', 'resource')) {
+                $db = ZfDbConnectionFactory::class::connection(
+                    IcingaResource::requireResourceConfig($name, 'db')
+                );
+                assert($db instanceof Db);
+                self::$db = $db;
+            } else {
+                throw new ConfigurationError(sprintf(
+                    'Found no "%s" in the "[%s]" section of %s',
+                    'resource',
+                    'db',
+                    Config::module(Defaults::MODULE_NAME)->getConfigFile()
+                ));
+            }
+        }
+        return self::$db;
+    }
+}
