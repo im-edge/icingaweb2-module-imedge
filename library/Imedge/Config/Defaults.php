1080a728c9427dec48baa64180c0589223d02b7f
Thomas Gelf <thomas@gelf.net>
2024-10-16 08:11:16 +0200

    Defaults: adjust module name

--- library/Imedge/Config/Defaults.php
+++ library/Imedge/Config/Defaults.php
@@ -5,6 +5,6 @@ namespace Icinga\Module\Imedge\Config;
 class Defaults
 {
     public const APPLICATION_NAME = 'IMEdge';
-    public const MODULE_NAME = 'IMEdge';
+    public const MODULE_NAME = 'imedge';
     public const IMEDGE_SOCKET = '/run/imedge/node.sock';
 }
