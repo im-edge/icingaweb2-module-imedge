6bd5ba292c0488b5368ba1fa544c1bca7cf328c0
Thomas Gelf <thomas@gelf.net>
2024-10-17 06:46:52 +0200

    IndexController: add IMEdge main page

--- configuration.php
+++ configuration.php
@@ -3,4 +3,10 @@
 use Icinga\Application\Modules\Module;
 
 /** @var Module $this */
+
+$section = $this->menuSection('IMEdge')
+    ->setUrl('imedge')
+    ->setPriority(80)
+    ->setIcon('services');
+
 $this->provideJsFile('combined.js');
