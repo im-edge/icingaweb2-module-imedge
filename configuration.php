7656de60c19e9465ddc088dabc4b797fa8e434d6
Thomas Gelf <thomas@gelf.net>
2024-10-17 07:58:37 +0200

    configuration: add menu entries for inventory

--- configuration.php
+++ configuration.php
@@ -9,4 +9,21 @@ $section = $this->menuSection('IMEdge')
     ->setPriority(80)
     ->setIcon('services');
 
+$section = $this->menuSection(N_('Inventory'))
+    ->setUrl('imedge/inventory')
+    ->setPriority(50)
+    ->setIcon('th-list');
+$section->add(N_('Devices'))
+    ->setUrl('imedge/snmp/devices')
+    ->setPriority(10);
+$section->add(N_('Credentials'))
+    ->setUrl('imedge/snmp/credentials')
+    ->setPriority(20);
+$section->add(N_('Discovery'))
+    ->setUrl('imedge/discovery/candidates')
+    ->setPriority(30);
+$section->add(N_('Monitoring Nodes'))
+    ->setUrl('imedge/nodes')
+    ->setPriority(40);
+
 $this->provideJsFile('combined.js');
